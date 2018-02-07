0ce9186679ef98dea217d45a97c8e7f812a5b3a2
권준동 <joondong@gwonjundong-ui-MacBook-Pro.local>
2018-02-08 02:27:47 +0900

    opkey_store,h annotation

--- led_button/main/include/opkey_store.h
+++ led_button/main/include/opkey_store.h
@@ -12,8 +12,34 @@
 #include "nvs.h"
 #include "nvs_flash.h"
 
+/*
+ * @ brief initialize key store
+ *
+ * This module is only responsible for saving and loadong operation key.
+ * To do this, this function set configuration related to nvs library.
+ *
+ * @ return		Reference "esp_err.h" and "nvs.h"
+ * */
 esp_err_t opkey_store_init();
+
+/*
+ * @ brief change the operation key
+ *
+ * It may need to check current key outside this module.
+ *
+ * @ param 	op_key	new key to change, including '\0'
+ *
+ * @ return
+ * */
 esp_err_t set_opkey(const char * op_key);
+
+/*
+ * @ brief get current operation key
+ *
+ * It may need to check current key outside this module.
+ *
+ * return 	current operation key string including '\0'
+ * */
 char * get_opkey();
 
 #endif /* MAIN_INCLUDE_OPKEY_STORE_H_ */
